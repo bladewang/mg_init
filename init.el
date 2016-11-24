be558f0aa1f0b0c63f76fad5e432d1dca121951e
bladewang <k.y.wang.1911@gmail.com>
2016-11-24 17:10:16 +0000

    init

---
+++ init.el
@@ -0,0 +1,37 @@
+
+;;; add by wky
+
+(menu-bar-mode -1)
+(setq auto-save-default nil)
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; (require 'linum)
+;; (set-face-foreground 'linum "magenta")
+(setq linum-format "%4d ")
+
+;; set charactor level diff
+(setq-default ediff-forward-word-function 'forward-char)
+
+(global-hl-line-mode 1)
+;; (set-face-background 'hl-line "color-19")
+(set-face-background 'hl-line "blue")
+(set-face-foreground 'hl-line "white")
+
+;; (set-face-background 'region "magenta")
+;; (set-face-background 'region "color-160")
+(set-face-background 'region "red")
+(set-face-foreground 'region "white")
+
+;;; config melpa
+(require 'package)
+(add-to-list
+ 'package-archives
+ '("melpa" . "http://melpa.org/packages/")
+ t)
+(package-initialize)
+
+(window-numbering-mode)
+
+;;; keys binding
+; (global-set-key (kbd "C-SPC") nil)
+; (global-set-key (kbd "C-@") 'set-mark-command)
