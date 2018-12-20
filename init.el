84f0d5b8f194742d65526312bc1ba68fbe40a38d
bladewang <blade@yy42.net>
2018-12-21 00:33:35 +0800

    custmize theme for magit

--- init.el
+++ init.el
@@ -64,8 +64,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-diff-added-highlight ((t (:background "black" :foreground "green"))))
- '(magit-diff-removed-highlight ((t (:background "black" :foreground "red")))))
+ '(magit-diff-added ((t (:background "black" :foreground "green"))))
+ '(magit-diff-added-highlight ((t (:background "blue" :foreground "white"))))
+ '(magit-diff-removed ((t (:background "black" :foreground "red"))))
+ '(magit-diff-removed-highlight ((t (:background "magenta" :foreground "white")))))
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
