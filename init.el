88591eac212d97d44a31fee1b91ffbcdbd48b994
bladewang <blade@yy42.net>
2018-12-24 05:04:52 +0800

    update

--- init.el
+++ init.el
@@ -35,6 +35,9 @@
 (window-numbering-mode)
 (global-undo-tree-mode)
 
+(load-theme 'afternoon t)
+(setq markdown-command "pandoc")
+
 (require 'pyim)
 (require 'pyim-basedict) ; 拼音词库设置，五笔用户 *不需要* 此行设置
 (pyim-basedict-enable)   ; 拼音词库，五笔用户 *不需要* 此行设置
@@ -53,12 +56,40 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#eaeaea" "#d54e53" "DarkOliveGreen3" "#e7c547" "DeepSkyBlue1" "#c397d8" "#70c0b1" "#181a26"))
  '(custom-safe-themes
    (quote
-    ("28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" default)))
+    ("2540689fd0bc5d74c4682764ff6c94057ba8061a98be5dd21116bf7bf301acfb" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" default)))
+ '(fci-rule-color "#14151E")
+ '(focus-follows-mouse nil)
  '(package-selected-packages
    (quote
-    (evil pyim markdown-mode window-numbering web-server w3m undo-tree sicp magit afternoon-theme))))
+    (evil pyim markdown-mode window-numbering web-server w3m undo-tree sicp magit afternoon-theme)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#d54e53")
+     (40 . "goldenrod")
+     (60 . "#e7c547")
+     (80 . "DarkOliveGreen3")
+     (100 . "#70c0b1")
+     (120 . "DeepSkyBlue1")
+     (140 . "#c397d8")
+     (160 . "#d54e53")
+     (180 . "goldenrod")
+     (200 . "#e7c547")
+     (220 . "DarkOliveGreen3")
+     (240 . "#70c0b1")
+     (260 . "DeepSkyBlue1")
+     (280 . "#c397d8")
+     (300 . "#d54e53")
+     (320 . "goldenrod")
+     (340 . "#e7c547")
+     (360 . "DarkOliveGreen3"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -67,7 +98,9 @@
  '(magit-diff-added ((t (:background "black" :foreground "green"))))
  '(magit-diff-added-highlight ((t (:background "blue" :foreground "white"))))
  '(magit-diff-removed ((t (:background "black" :foreground "red"))))
- '(magit-diff-removed-highlight ((t (:background "magenta" :foreground "white")))))
+ '(magit-diff-removed-highlight ((t (:background "magenta" :foreground "white"))))
+ '(magit-hash ((t (:foreground "magenta"))))
+ '(magit-section-highlight ((t (:background "black")))))
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
