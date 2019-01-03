3ef92036e29730ab4537695142c76bd85d714bf3
bladewang <blade@yy42.net>
2019-01-04 01:17:32 +0800

    update

--- init.el
+++ init.el
@@ -95,6 +95,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(completions-annotations ((t (:foreground "cyan"))))
  '(magit-diff-added ((t (:background "black" :foreground "green"))))
  '(magit-diff-added-highlight ((t (:background "blue" :foreground "white"))))
  '(magit-diff-removed ((t (:background "black" :foreground "red"))))
