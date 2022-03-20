707bd4ea96343f6efcae53257776d5219a726bd9
root <root@bladewang>
2022-03-20 15:45:32 +0800

    list installed packages;

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;;; add by wky
 
 (menu-bar-mode -1) ;; M-x tmm-menubar ; to visit menu options
-(global-hl-line-mode 1)
+;(global-hl-line-mode 1)
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
@@ -29,6 +29,21 @@
  '("melpa" . "http://melpa.org/packages/")
  t)
 (package-initialize)
+
+;;; installed packages
+;  ace-window         20200606.1259 installed             Quickly switch windows.
+;  afternoon-theme    20140104.1859 installed             Dark color theme with a deep blue background
+;  ag                 20201031.2202 installed             A front-end for ag ('the silver searcher'), the C ack replacement.
+;  counsel            20211230.1909 installed             Various completion functions using Ivy
+;  dired-single       20211101.2319 installed             Reuse the current dired buffer to visit a directory
+;  elpy               20220220.2059 installed             Emacs Python Development Environment
+;  evil               20220309.2216 installed             Extensible Vi layer for Emacs.
+;  j-mode             20171224.1856 installed             Major mode for editing J programs
+;  magit              20220319.1030 installed             A Git porcelain inside Emacs.
+;  sicp               20200512.1137 installed             Structure and Interpretation of Computer Programs in info format
+;  undo-tree          0.8.2         installed             Treat undo history as a tree
+;  vlf                20191126.2250 installed             View Large Files
+;  w3m                20211122.335  installed             an Emacs interface to w3m
 
 ;;; for emacsclient
 ;;; alias em='emacsclient -t -a "emacs -Q -l ~/mg_init/init.el " '
