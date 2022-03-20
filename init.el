302f2bbb0888d17f976183bb1ab47368febb4b83
root <root@bladewang>
2022-03-21 05:19:10 +0800

    more comment about dired

--- init.el
+++ init.el
@@ -54,7 +54,7 @@
 ; (global-set-key (kbd "C-@") 'set-mark-command)
 (put 'upcase-region 'disabled nil)
 
-;; learned from xahlee
+;; learned from xahlee ;; or just (require 'dired-single)
 (require 'dired )
 (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file) ; was dired-advertised-find-file
 (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file ".."))) ; was dired-up-directory
