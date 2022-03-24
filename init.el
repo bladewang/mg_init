278891a4bb15e393f773da46b9227b8aae8ab709
root <root@bladewang>
2022-03-25 04:08:05 +0800

    not worked yet

--- init.el
+++ init.el
@@ -2,72 +2,77 @@
 ;;; add by wky
 
 (menu-bar-mode -1) ;; M-x tmm-menubar ; to visit menu options
-
-
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (setq-default auto-save-default  nil
               make-backup-files  nil
               linum-format  "%4d "
               ediff-forward-word-function  'forward-char
-	      undo-tree-auto-save-history  nil
               diff-switches "-u")
 
 ;;; (setq-default default-case-fold-search nil) ;;; :set ignorecase
 
-;(global-hl-line-mode 1)
-;; (set-face-background 'hl-line "color-19")
-;(set-face-background 'hl-line "blue")
-;(set-face-foreground 'hl-line "white")
-
-;; (set-face-background 'region "color-160")
-(set-face-background 'region "red")
-(set-face-foreground 'region "white")
-
 ;;; config melpa
 (require 'package)
-(add-to-list
- 'package-archives
- '("melpa" . "http://melpa.org/packages/")
- t)
+(setq package-archives
+  '(("melpa"    . "http://melpa.org/packages/")))
+;;    ("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+;;    ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+
 (package-initialize)
 
-;;; installed packages
-;  ace-window                Quickly switch windows.
-;  afternoon-theme           Dark color theme with a deep blue background
-;  ag                        A front-end for ag ('the silver searcher'), the C ack replacement.
-;  wgrep-ag                  Writable ag buffer and apply the changes to files
-;  counsel                   Various completion functions using Ivy
-;  dired-single              Reuse the current dired buffer to visit a directory
-;  elpy                      Emacs Python Development Environment
-;  evil                      Extensible Vi layer for Emacs.
-;  j-mode                    Major mode for editing J programs
-;  magit                     A Git porcelain inside Emacs.
-;  sicp                      Structure and Interpretation of Computer Programs in info format
-;  undo-tree                 Treat undo history as a tree
-;  vlf                       View Large Files
-;  w3m                       an Emacs interface to w3m
+(when (not (package-installed-p 'use-package))
+  (package-install 'use-package))
+
+
+(use-package counsel
+  :ensure
+  :commands counsel-mode)
+
+(use-package undo-tree
+  :ensure
+  :init
+  (setq undo-tree-auto-save-history  nil)
+  :commands global-undo-tree-mode)
+
+(defvar my/packages
+  '(ag                    ;A front-end for ag ('the silver searcher'), the C ack replacement.
+    ;ace-window            ;Quickly switch windows.
+    ;undo-tree             ;Treat undo history as a tree
+    wgrep-ag              ;Writable ag buffer and apply the changes to files
+    dired-single          ;Reuse the current dired buffer to visit a directory
+    evil                  ;Extensible Vi layer for Emacs.
+    afternoon-theme       ;Dark color theme with a deep blue background
+    monokai-theme         ;A fruity color theme for Emacs.
+    ;use-package           ;A configuration macro for simplifying your .emacs
+    ;counsel               ;Various completion functions using Ivy
+    magit                 ;A Git porcelain inside Emacs.
+    sicp                  ;Structure and Interpretation of Computer Programs in info format
+    ;elpy                  ;Emacs Python Development Environment
+    j-mode                ;Major mode for editing J programs
+    vlf                   ;View Large Files
+    w3m                   ;an Emacs interface to w3m
+    qrencode              ;QRCode encoder
+    ))
 
 ;;; for emacsclient
 ;;; alias em='emacsclient -t -a "emacs -Q -l ~/mg_init/init.el " '
 (server-start)
 
-;;; keys binding
-; (global-set-key (kbd "C-SPC") nil)
-; (global-set-key (kbd "C-@") 'set-mark-command)
 (put 'upcase-region 'disabled nil)
 
 ;; learned from xahlee ;; or just (require 'dired-single)
 (require 'dired )
 (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file) ; was dired-advertised-find-file
 (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file ".."))) ; was dired-up-directory
+;; press "o" to open file in another window
+
 (require 'view)
 (define-key view-mode-map (kbd "e") 'View-scroll-line-forward)
 
 ;;; use below codes after package-install undoo-tree, ace-window, ivy, counsel, elpy
-(global-undo-tree-mode)
 (global-set-key (kbd "M-o") 'ace-window)
-(ivy-mode)
+
 (setq python-shell-interpreter "python3")
 ;; when editing .py file (elpy-enalbe)
 (load-theme 'afternoon 1)
