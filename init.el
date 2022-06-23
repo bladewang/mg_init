a636e0c8c4428a96097f9920344be950b68f4f18
bladewang <k.y.wang.1911@gmail.com>
2022-06-24 00:56:34 +0800

    update themes

--- init.el
+++ init.el
@@ -119,6 +119,7 @@
     monokai-alt-theme     ;Theme with a dark background. Based on sublime monokai theme.
     nimbus-theme          ;An awesome dark theme
     phoenix-dark-mono-theme ;Monochromatic version of the Phoenix theme
+    tao-theme             ; package provides two parametrized uncoloured color themes for Emacs: tao-yin and tao-yang.
     darkburn-theme        ;A not-so-low contrast color theme for Emacs.
     colonoscopy-theme     ;an Emacs 24 theme based on Colonoscopy (tmTheme)
     material-theme        ;Theme based on the colors of the Google Material Design
