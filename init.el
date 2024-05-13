9d9fdb91ce8e9a0a322feff34104c2f166780662
bladewang <k.y.wang.1911@gmail.com>
2024-05-13 22:53:49 +0800

    fix j-mode installation with LdBeth version

--- init.el
+++ init.el
@@ -55,6 +55,8 @@
 (when (not (package-installed-p 'quelpa-use-package))
   (package-install 'quelpa-use-package))
 
+(require 'quelpa-use-package)
+
 (use-package counsel
   :ensure t
   :config
@@ -68,7 +70,7 @@
   ("C-M-s" . 'swiper-isearch))
 
 (use-package j-mode
-  :pin melpa
+  :quelpa (j-mode :fetcher github :repo "LdBeth/j-mode")
   :config
   ;; for j90x
   (setq j-console-cmd "jconsole"))
