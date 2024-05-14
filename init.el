ee728e7ef82a04b0d828183dad4b763df722419a
bladewang <k.y.wang.1911@gmail.com>
2024-05-14 14:28:22 +0800

    update theme

--- init.el
+++ init.el
@@ -139,7 +139,6 @@
     spacemacs-theme       ;Color theme with a dark and light versions
     nano-theme            ;N Λ N O theme
     monokai-theme         ;A fruity color theme for Emacs.
-    moe-theme             ;colorful eye-candy theme. Moe, moe, kyun!
     darktooth-theme       ; the darkness... it watches
     ample-theme           ;Dark Theme for Emacs
     nimbus-theme          ;An awesome dark theme
