f57805eeb6be7675bb461ab81900e924c81525ec
bladewang <k.y.wang.1911@gmail.com>
2025-12-24 01:16:55 +0900

    remove needless code

--- init.el
+++ init.el
@@ -402,9 +402,4 @@
 	      wgrep-ag zoom)))
 (put 'scroll-left 'disabled nil)
 (put 'magit-diff-edit-hunk-commit 'disabled nil)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(bqn-default ((t (:family "Iosevka"))) t))
+
